a05c63a29a20509710f8d5e2cf418335a23b39e2
mairasirino <mairacarv@gmail.com>
2021-09-19 14:25:26 -0300

    Curso de introdução javascript

--- Curso Introdução ao Javascript/js/main.js
+++ Curso Introdução ao Javascript/js/main.js
@@ -1,4 +1,62 @@
-var fruta = { nome: 'maça', cor: 'vermelha' }
+/*function botao() {
+  document.getElementById('agradecimento').innerHTML = 'Obrigado por clicar'
+  console.log(document.getElementById('Agradecimento'))
+}
+
+/*function botao() {
+  alert('Obrigado por clicar')
+}
+
+/*function soma(n1, n2) {
+  return n1 + n2
+}
+
+function validaIdade(idade) {
+  var validar
+  if (idade >= 18) {
+    var validar = true
+  } else {
+    validar = false
+  }
+  return validar
+}
+
+var idade = prompt('Qual sua idade?')
+console.log(validaIdade(idade))
+
+//alert(soma(5, 10));
+
+/*function soma(n1, n2) {
+  return n1 + n2
+}
+
+function setReplace(frase, nome, novo_nome) {
+  return frase.replace(nome, novo_nome)
+}
+
+alert(soma(5, 10))
+alert(setReplace('Vai Japão', 'Japão', 'Brasil'))
+
+/*var d = new Date()
+alert(d)
+/*var count = 0
+for (count; count <= 5; count++) {
+  alert(count)
+}
+/*var count = 0
+while (count < 5) {
+  console.log(count)
+  count++
+}
+
+/*var idade = prompt('Qual sua idade?')
+if (idade >= 18) {
+  alert('Maior de idade')
+} else {
+  alert('Menor de idade')
+}
+
+//var fruta = { nome: 'maça', cor: 'vermelha' }
 
 /*var nome = 'Maira Sirino'
 
