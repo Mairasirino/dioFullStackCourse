94418280617076310dc9ab886407e4ec63682938
mairasirino <mairacarv@gmail.com>
2021-09-18 22:15:01 -0300

    Curso introdução javascript

--- Curso Introdução ao Javascript/README.md
+++ Curso Introdução ao Javascript/README.md
@@ -1,1 +1,6 @@
 # Anotações Curso Introdução ao Javascript
+
+#### Document Object Model
+
+Javascript pode alterar todos os elementos do HTML
+Javascript pode alterar todos os elementos do CSS
---
+++ Curso Introdução ao Javascript/js/main.js
@@ -0,0 +1,12 @@
+var fruta = { nome: 'maça', cor: 'vermelha' }
+
+/*var nome = 'Maira Sirino'
+
+//alert('Bem vindo ' + nome)
+
+//Arrays
+
+var lista = ['maçã', 'pera', 'laranja']
+lista.push('Doce')
+console.log(lista)
+*/
